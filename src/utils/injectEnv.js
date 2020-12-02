b9102ab8e3b4a23f662b3c4cf17309859bde47f8
Davit Vardanyan <jedavard@gmail.com>
2020-12-02 05:03:10 +0400

    Setup/ci (#1)
    * update
    
    * update
    
    * actions

--- src/utils/injectEnv.js
+++ src/utils/injectEnv.js
@@ -1,6 +1,6 @@
 const { config } = require('dotenv');
 
-const environment = process.env.NODE_ENV?.toLowerCase() || 'development';
+const environment = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : 'development';
 
 if (environment === 'development') config({ path: '.env.development' });
 if (environment === 'test') config({ path: '.env.test' });
